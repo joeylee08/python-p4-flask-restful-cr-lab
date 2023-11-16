3af82b9479b5a5f66d5580b74ae4f7470dd6eced
Joseph <josephylee72@gmail.com>
2023-11-15 21:38:13 -0800

    completed flask restful CR lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,27 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response = [item.to_dict() for item in Plant.query.all()]
+        return make_response(response, 200)
+    
+    def post(self):
+        data = request.get_json()
+        new_item = Plant(**data)
+
+        db.session.add(new_item)
+        db.session.commit()
+
+        return make_response(new_item.to_dict(), 201)
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        response = Plant.query.filter_by(id = id).first().to_dict()
+        return make_response(response, 200)
         
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
